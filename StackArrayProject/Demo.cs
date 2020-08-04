45c9a253a2e06fcdcf89abd25ce4bd915be8012d
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-04 18:41:50 +0530

    Initial commit

---
+++ StackArrayProject/Demo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace StackArrayProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+            StackA st = new StackA(8);
+            while (true)
+            {
+                Console.WriteLine("1.Push an element on the stack");
+                Console.WriteLine("2.Pop an element to the stack");
+                Console.WriteLine("3.Display the top element");
+                Console.WriteLine("4.Display all stack elements");
+                Console.WriteLine("5.Display size of the stack");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 6)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the element to be pushed:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        st.Push(x);
+                        break;
+                    case 2:
+                        x=st.Pop();
+                        Console.WriteLine("Popped element is:"+x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Element at the top:" + st.Peek());
+                        break;
+                    case 4:
+                        st.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Size of the stack:" + st.size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ StackArrayProject/StackA.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackArrayProject
+{
+    class StackA
+    {
+        private int[] stackArray;
+        private int top;
+        public StackA()
+        {
+            stackArray = new int[10];
+            top = -1;
+        }
+        public StackA(int maxSize)
+        {
+            stackArray = new int[maxSize];
+            top = -1;
+
+        }
+        public int size()
+        {
+            return top + 1;
+        }
+        public bool IsEmpty()
+        {
+            return (top == -1);
+        }
+        public bool IsFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+        public void Push(int x)
+        {
+            if (IsFull()){
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            top = top + 1;
+            stackArray[top] = x;
+        }
+        public int Pop()
+        {
+            int x;
+            if (IsEmpty())
+                throw new System.InvalidOperationException("stack underflow");
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        } 
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new System.InvalidOperationException("stack underflow");
+            return stackArray[top];
+        }
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("Stack is :");
+            for (int i = top; i >= 0; i--)
+                Console.WriteLine(stackArray[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
